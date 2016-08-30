6355467ebbc07bb8a8093370ef1fdbbca1c642d1
zwq <920415463@qq.com>
2016-08-30 17:10:28 +0800

    导入

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @Description:
+ * @author: Lucifer
+ * @date: 2016/8/30 17:04
+ */
+public class Test {
+
+    public static void main(String[] args){
+        System.out.println("abc");
+    }
+}
