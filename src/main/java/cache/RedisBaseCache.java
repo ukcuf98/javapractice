29ccb217f7028fed120a69d942416035708fab18
zwq <920415463@qq.com>
2017-09-06 14:26:19 +0800

    redis集群调用方式

---
+++ src/main/java/cache/RedisBaseCache.java
@@ -0,0 +1,149 @@
+package cache;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.JedisCluster;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * <p>Title: RedisBaseCache.java</p>
+ *
+ * <p>Package: com.wiwj.app.cache</p>
+ *
+ * <p>Description: Redis缓存基类</p>
+ *
+ * @author: 张立成
+ *
+ * @date: 2017年2月28日 下午12:42:31
+ *
+ * @version: 1.0
+ */
+public abstract class RedisBaseCache {
+
+	private static final Logger logger = LoggerFactory.getLogger(RedisBaseCache.class);
+	private static JedisCluster jedisCluster = null;
+
+	static {
+		try {
+			Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
+        jedisClusterNodes.add(new HostAndPort("10.2.1.201", 6376));
+			jedisCluster = new JedisCluster(jedisClusterNodes);
+		} catch (Exception e) {
+			logger.error("RedisBaseCache Static Error:" + e.getMessage(), e);
+		}
+	}
+
+	protected synchronized static JedisCluster getJedis(){
+		try {
+			if (jedisCluster  == null) {
+				Set<HostAndPort> jedisClusterNodes = new HashSet<HostAndPort>();
+				jedisClusterNodes.add(new HostAndPort("10.2.1.201", 6376));
+				jedisCluster = new JedisCluster(jedisClusterNodes);
+			}
+			return jedisCluster;
+		} catch (Exception ex) {
+			logger.error("获取Redis集群实例错误:" + ex.getMessage(), ex);
+			return null;
+		}
+
+	}
+
+	/**
+	 * 设置带有过期时间的String型值，单位:秒
+	 * @param key
+	 * @param seconds
+	 * @param value
+	 * @return String
+	 */
+	public String putString(String key, String value, int seconds) {
+		String result = null;
+		try {
+			result = getJedis().setex(key, seconds, value);
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+
+	/**
+	 * 判断一个key是否存在
+	 * @param key
+	 * @return Boolean
+	 */
+	protected Boolean exists(String key) {
+		Boolean result = false;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.exists(key);
+		} catch (Exception e) {
+			return false;
+		}
+		return result;
+	}
+
+	/**
+	 * 在某个时间点失效(以秒为单位)
+	 * @param key
+	 * @param seconds
+	 * @return Long
+	 */
+	protected Long expire(String key, int seconds) {
+		Long result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.expire(key, seconds);
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+
+	/**
+	 * 在某个Unix格式时间点失效
+	 * @param key
+	 * @param unixTime
+	 * @return Long
+	 */
+	protected Long expireAt(String key, long unixTime) {
+		Long result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.expireAt(key, unixTime);
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+
+	/**
+	 * 返回某个key剩余生存时间
+	 * @param key
+	 * @return Long
+	 */
+	protected Long ttl(String key) {
+		Long result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.ttl(key);
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+}
---
+++ src/main/java/cache/RedisCache.java
@@ -0,0 +1,465 @@
+package cache;
+
+import redis.clients.jedis.JedisCluster;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * <p>Title: RedisCache.java</p>
+ *
+ * <p>Package: com.wiwj.app.cache</p>
+ *
+ * <p>Description: Redis缓存单例类</p>
+ *
+ * @author: 张立成
+ *
+ * @date: 2017年2月28日 下午12:46:34
+ *
+ * @version: 1.0
+ */
+public class RedisCache extends RedisBaseCache {
+
+	private final static RedisCache instance = new RedisCache();
+
+	private RedisCache() {}
+
+	public static RedisCache getInstance() {
+		return instance;
+	}
+
+	/**
+	 * 设置String型值
+	 * @param key
+	 * @param value
+	 * @return String
+	 */
+	public String putString(String key, String value) {
+		String result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.set(key, value);
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+
+	/**
+	 * 设置带有过期时间的String型值，单位:秒
+	 * @param key
+	 * @param seconds
+	 * @param value
+	 * @return String
+	 */
+	public String putString(String key, String value, int seconds) {
+		String result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.setex(key, seconds, value);
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+
+	/**
+	 * 获取String型值
+	 * @param key
+	 * @return String
+	 */
+	public String getString(String key) {
+		String result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.get(key);
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+
+	/**
+	 * 设置Map型值
+	 * @param key
+	 * @param map
+	 * @return String
+	 */
+	public String putMap(String key, Map<String, String> map) {
+		String result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.hmset(key, map);
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+
+	/**
+	 * 设置Map型值
+	 * @param key
+	 * @param map
+	 * @param seconds 过期时间(秒)
+	 * @return String
+	 */
+	public String putMap(String key, Map<String, String> map, int seconds) {
+		String result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.hmset(key, map);
+			client.expire(key, seconds);
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+
+	/**
+	 * 获取Map型值
+	 * @param key
+	 * @return Map<String, String>
+	 */
+	public Map<String, String> getMap(String key) {
+		Map<String, String> result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.hgetAll(key);
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+
+	/**
+	 * 在List头部增加元素
+	 * @param key
+	 * @param string
+	 * @return Long
+	 */
+	public Long putListOnLeft(String key, String string) {
+		Long result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.lpush(key, string);
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+
+	/**
+	 * 在List头部增加元素
+	 * @param key
+	 * @param string
+	 * @param seconds 过期时间(秒)
+	 * @return Long
+	 */
+	public Long putListOnLeft(String key, String string, int seconds) {
+		Long result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.lpush(key, string);
+			client.expire(key, seconds);
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+
+	/**
+	 * 在List尾部增加元素
+	 * @param key
+	 * @param string
+	 * @return Long
+	 */
+	public Long putListOnRight(String key, String string) {
+		Long result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.rpush(key, string);
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+
+	/**
+	 * 在List尾部增加元素
+	 * @param key
+	 * @param string
+	 * @param seconds 过期时间(秒)
+	 * @return Long
+	 */
+	public Long putListOnRight(String key, String string, int seconds) {
+		Long result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.rpush(key, string);
+			client.expire(key, seconds);
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+
+	/**
+	 * 获取List型值
+	 * @param key
+	 * @param start
+	 * @param end
+	 * @return List<String>
+	 */
+	public List<String> getList(String key, long start, long end) {
+		List<String> result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.lrange(key, start, end);
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+
+	/**
+	 * 设置Set型值
+	 * @param key
+	 * @param string
+	 * @return Long
+	 */
+	public Long putSet(String key, String string) {
+		Long result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.sadd(key, string);
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+
+	/**
+	 * 设置Set型值
+	 * @param key
+	 * @param string
+	 * @param seconds 过期时间(秒)
+	 * @return Long
+	 */
+	public Long putSet(String key, String string, int seconds) {
+		Long result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.sadd(key, string);
+			client.expire(key, seconds);
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+
+	/**
+	 * 获取Set型值
+	 * @param key
+	 * @return Set<String>
+	 */
+	public Set<String> getSet(String key) {
+		Set<String> result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.smembers(key);
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+
+//	/**
+//	 * 设置Byte型值
+//	 * @param key
+//	 * @param value
+//	 * @return String
+//	 */
+//	public String putByte(byte[] key, byte[] value) {
+//		String result = null;
+//		JedisCluster client = getJedis();
+//		if (client == null) {
+//			return result;
+//		}
+//		try {
+//			result = client.set(key, value);
+//		} catch (Exception e) {
+//			return null;
+//		}
+//		return result;
+//	}
+//
+//	/**
+//	 * 设置Byte型值
+//	 * @param key
+//	 * @param value
+//	 * @param value 过期时间(秒)
+//	 * @return String
+//	 */
+//	public String putByte(byte[] key, byte[] value, int seconds) {
+//		String result = null;
+//		JedisCluster client = getJedis();
+//		if (client == null) {
+//			return result;
+//		}
+//		try {
+//			result = client.set(key, value);
+//			client.expire(key, seconds);
+//		} catch (Exception e) {
+//			return null;
+//		}
+//		return result;
+//	}
+//
+//	/**
+//	 * 获取Byte型值
+//	 * @param key
+//	 * @return byte[]
+//	 */
+//	public byte[] getByte(byte[] key) {
+//		byte[] result = null;
+//		JedisCluster client = getJedis();
+//		if (client == null) {
+//			return result;
+//		}
+//		try {
+//			result = client.get(key);
+//		} catch (Exception e) {
+//			return null;
+//		}
+//		return result;
+//	}
+
+	/**
+	 * 删除key的值
+	 * @param key
+	 * @return Long
+	 */
+	public Long del(String key) {
+		Long result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.del(key);
+		} catch (Exception e) {
+			return null;
+		} finally {
+			if (null != client) {
+				client.close();
+			}
+		}
+		return result;
+	}
+
+//	/**
+//	 * 删除字节key的值
+//	 * @param key
+//	 * @return Long
+//	 */
+//	public Long del(byte[] key) {
+//		Long result = null;
+//		JedisCluster client = getJedis();
+//		if (client == null) {
+//			return result;
+//		}
+//		try {
+//			result = client.del(key);
+//		} catch (Exception e) {
+//			return null;
+//		}
+//		return result;
+//	}
+
+	/**
+	 * 清空所有
+	 * @return String
+	 */
+	@SuppressWarnings("deprecation")
+	public String flushAll() {
+		String result = null;
+		JedisCluster client = getJedis();
+		if (client == null) {
+			return result;
+		}
+		try {
+			result = client.flushAll();
+		} catch (Exception e) {
+			return null;
+		}
+		return result;
+	}
+
+	public Boolean exists(String key) {
+		return super.exists(key);
+	}
+
+	public Long expire(String key, int seconds) {
+		return super.expire(key, seconds);
+	}
+
+	public Long expireAt(String key, long unixTime) {
+		return super.expireAt(key, unixTime);
+	}
+
+	public Long ttl(String key) {
+		return super.ttl(key);
+	}
+
+	public static void main(String[]args){
+		RedisCache redisCache = RedisCache.getInstance();
+		String key = "abc";
+		String setResult = redisCache.putString(key, "33312");
+//		String result = redisCache.getString(key);
+//		System.out.println("setResult="+setResult+"=="+result);
+	}
+}
