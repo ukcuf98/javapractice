7785fcf00a913f1a4899543685d6fec5b84891da
zwq <920415463@qq.com>
2017-09-06 14:32:53 +0800

    临时保存

--- src/main/java/CodecSha1.java
+++ src/main/java/CodecSha1.java
@@ -1,5 +1,9 @@
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.codec.digest.DigestUtils;
+import sun.misc.BASE64Decoder;
 
+import javax.imageio.ImageIO;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -12,6 +16,14 @@ import java.util.List;
 public class CodecSha1 {
 
     public static void main(String[] args){
+        String blank = new String(Base64.encodeBase64("".getBytes()));
+        System.out.println(blank);
+        System.out.println(Base64.decodeBase64(blank));
+        String test = getImageStr("E://663145.jpg");
+        System.out.println(test);
+        GenerateImage(test);
+        String test2 = getStrFromPic("E://663145.jpg");
+        System.out.println(test2);
         List<String> list = new ArrayList<String>();
         list.add("abc");
         list.add("15236");
@@ -19,5 +31,71 @@ public class CodecSha1 {
         Collections.sort(list);
         String result = DigestUtils.shaHex(list.get(0)+list.get(1)+list.get(2));
         System.out.println(result);
+    }
+
+    public static String getImageStr(String path){
+//        File file = new File(path);
+        InputStream in = null;
+        byte[]data = null;
+        try {
+            in = new FileInputStream(path);
+            data = new byte[in.available()];
+            in.read(data);
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new String(Base64.encodeBase64(data));
+    }
+
+    public static String getStrFromPic(String path){
+        StringBuffer result = new StringBuffer();
+        File file = new File(path);
+        InputStream in = null;
+        try {
+//            System.out.println("以字节为单位读取文件内容，一次读多个字节：");
+            byte [] bArr = new byte[100];
+            int b;
+            in = new FileInputStream(file);
+//            System.out.println("当前字节输入流中的字节数为:" + in.available());
+            while ((b=in.read(bArr))!=-1){
+                result.append(new String(bArr));
+            }
+            in.close();
+            return new String(Base64.encodeBase64(result.toString().getBytes()));
+        }catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public static boolean GenerateImage(String imgStr)
+    {   //对字节数组字符串进行Base64解码并生成图片
+        if (imgStr == null) //图像数据为空
+            return false;
+        try
+        {
+            //Base64解码
+            byte[] b = Base64.decodeBase64(imgStr);
+            for(int i=0;i<b.length;++i)
+            {
+                if(b[i]<0)
+                {//调整异常数据
+                    b[i]+=256;
+                }
+            }
+            //生成jpeg图片
+            String imgFilePath = "e://222.jpg";//新生成的图片
+            OutputStream out = new FileOutputStream(imgFilePath);
+            out.write(b);
+            out.flush();
+            out.close();
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
     }
 }
--- src/main/java/command/CommandTest.java
+++ src/main/java/command/CommandTest.java
@@ -1,9 +1,12 @@
 package command;
 
+import org.apache.commons.lang3.StringEscapeUtils;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
+
 
 /**
  * @Description:
@@ -12,19 +15,58 @@ import java.io.LineNumberReader;
  */
 public class CommandTest {
     public static void main(String[] args) {
-        for (int i = 10; i < 100; i++) {
-            String address = "10.1.71.";
-            address = address + i;
-//            System.out.println(address);
-            try {
-                boolean flag = get(address);
-                if (flag) {
-                    System.out.println("====" + address);
-                }
-            } catch (Exception e) {
+        String str = "\\U591a\\U4e00\\U5206\\U771f\\U8bda\\Uff0c\\U5c11\\U4e00\\U70b9\\U5957\\U8def\\Ud83d\\Ude14";
+//        String test = StringEscapeUtils.unescapeJava(str);
+        try {
+            String test = new String(str.getBytes(),"UTF-8");
+            System.out.println(test.trim());
+        }catch (Exception e){
 
+        }
+
+
+//        String test = decode(str);
+
+//        for (int i = 1; i < 100; i++) {
+//            String address = "10.1.71.";
+//            address = address + i;
+////            System.out.println(address);
+//            try {
+//                boolean flag = get(address);
+//                if (flag) {
+//                    System.out.println("====" + address);
+//                }
+//            } catch (Exception e) {
+//
+//            }
+//        }
+    }
+
+    public static String decode(String unicodeStr) {
+        if (unicodeStr == null) {
+            return null;
+        }
+        StringBuffer retBuf = new StringBuffer();
+        int maxLoop = unicodeStr.length();
+        for (int i = 0; i < maxLoop; i++) {
+            if (unicodeStr.charAt(i) == '\\') {
+                if ((i < maxLoop - 5)
+                        && ((unicodeStr.charAt(i + 1) == 'u') || (unicodeStr
+                        .charAt(i + 1) == 'U')))
+                    try {
+                        retBuf.append((char) Integer.parseInt(
+                                unicodeStr.substring(i + 2, i + 6), 16));
+                        i += 5;
+                    } catch (NumberFormatException localNumberFormatException) {
+                        retBuf.append(unicodeStr.charAt(i));
+                    }
+                else
+                    retBuf.append(unicodeStr.charAt(i));
+            } else {
+                retBuf.append(unicodeStr.charAt(i));
             }
         }
+        return retBuf.toString();
     }
 
     public static boolean get(String address) throws IOException {
