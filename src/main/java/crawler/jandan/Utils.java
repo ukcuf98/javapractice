1343e03def38587a2fac2b2988602dd5b5060ca5
zwq <920415463@qq.com>
2018-03-29 14:16:00 +0800

    数字转汉字

--- src/main/java/crawler/jandan/Utils.java
+++ src/main/java/crawler/jandan/Utils.java
@@ -1,5 +1,7 @@
 package crawler.jandan;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -13,6 +15,9 @@ import java.io.InputStreamReader;
  * @date: 2016/11/22 10:54
  */
 public class Utils {
+	private static String[] number2Chinese_s1 = { "零", "一", "二", "三", "四", "五", "六", "七", "八", "九" };
+	private static String[] number2Chinese_s2 = { "十", "百", "千", "万", "十", "百", "千", "亿", "十", "百", "千" };
+
 	public static void writeToFile(InputStream in, String path) {
 		File file = new File(path);
 		System.out.println("是否为文件：" + file.isFile());
@@ -52,5 +57,41 @@ public class Utils {
 		}
 		return sb.toString();
 
+	}
+
+	/**
+	 * 数字转汉字
+	 * @param number
+	 * @param suffix
+	 * @return
+	 */
+	public static String number2Chinese(String number, String suffix){
+		if(StringUtils.isBlank(number)){
+			return "";
+		}
+//		if(number.equals("99")&& !suffix.equals("年")){
+//			return "多"+suffix;
+//		}
+		StringBuilder result = new StringBuilder();
+		int n = number.length();
+		for (int i = 0; i < n; i++) {
+			int num = number.charAt(i) - '0';
+			if (i != n - 1 && num != 0) {
+				if(num<0){
+					System.out.println("number:"+number+"suffix:"+suffix);
+				}
+				result.append(number2Chinese_s1[num]);
+				result.append(number2Chinese_s2[n - 2 - i]);
+			} else {
+				result.append(number2Chinese_s1[num]);
+			}
+//			if(result.toString().equals("一十")){//习惯特殊处理
+//				result.append(number2Chinese_s2[0]);
+//			}
+		}
+		if(StringUtils.isNotBlank(result)){
+			result.append(suffix);
+		}
+		return result.toString();
 	}
 }
